5953cf34aa4f9f7fe6d1dbb7db6dbf9d887ee1f7
Ayomide Jones <ayormeday@gmail.com>
2019-10-18 00:23:08 +0100

    creating the onchange and onsubmit function

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,36 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
+
+const initialMovieFormState = {
+  id: null,
+  title: "",
+  director: "",
+	metascore: "",
+	stars: ""
+};
+
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const[moviesForm, setMoviesForm] = useState(initialMovieFormState);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+  
+  const onFormChange = e => {
+    e.preventDefault();
+    const input = {[e.target.name]: e.target.value};
+    setMoviesForm({
+      ...moviesForm,
+      ...input
+    })
+  };
+
+  const onSubmitMovie = e => {
+    
+  }
 
   return (
     <>
